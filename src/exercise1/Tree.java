e57a993533d588c492615149c5332229d58b90d8
zlatinas <>
2021-10-14 08:15:26 +0000

    z

---
+++ src/exercise1/Tree.java
@@ -0,0 +1,17 @@
+package exercise1;
+
+public class Tree {
+
+    public static void main(String[] arg) {
+        System.out.println("    * ");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+        System.out.println("   ***");
+
+        
+        
+    }
+
+}
